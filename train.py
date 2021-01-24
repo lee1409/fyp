39ed757db171e5af0ad1cc79322ba10b07cd3e5a
lee1409 <leerun1409@gmail.com>
2021-01-24 11:05:45 +0800

    edit train files

--- train.py
+++ train.py
@@ -1,21 +1,61 @@
 from config.default import _C
-from util.train import Trainer
+from detectron2.config import get_cfg
+from detectron2.engine import default_argument_parser, default_setup, hooks, launch
+from util.trainer import Trainer
+from detectron2.checkpoint import DetectionCheckpointer
+import detectron2.utils.comm as comm
 
-cfg = _C.clone()
-cfg.merge_from_file("./base.yaml")
-cfg.DATASETS.TRAIN = ("text_train",)
-cfg.DATASETS.TEST = ()
-cfg.DATALOADER.NUM_WORKERS = 2
-cfg.SOLVER.BASE_LR = 0.002  # pick a good LearningRate
-cfg.SOLVER.MAX_ITER = 20000  #No. of iterations
-cfg.SOLVER.IMS_PER_BATCH = 6
-cfg.MODEL.FCOS.NUM_CLASSES = 1 # Only text class
-cfg.INPUT.HFLIP_TRAIN = False
-cfg.MODEL.FCOS.YIELD_PROPOSAL = False
-cfg.MODEL.META_ARCHITECTURE = "OneStageDetector"
-cfg.freeze()
 
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = Trainer(cfg)
-trainer.resume_or_load(resume=True)
-trainer.train()
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = get_cfg()
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    default_setup(cfg, args)
+    return cfg
+
+
+def main(args):
+    cfg = setup(args)
+
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model)
+        if cfg.TEST.AUG.ENABLED:
+            res.update(Trainer.test_with_TTA(cfg, model))
+        if comm.is_main_process():
+            # TODO produce images
+            pass
+        return res
+
+    """
+    If you'd like to do anything fancier than the standard training logic,
+    consider writing your own training loop (see plain_train_net.py) or
+    subclassing the trainer.
+    """
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=args.resume)
+    if cfg.TEST.AUG.ENABLED:
+        trainer.register_hooks(
+            [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
+        )
+    return trainer.train()
+
+
+if __name__ == "__main__":
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
